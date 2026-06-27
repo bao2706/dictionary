b6a6119c874236327077eb104068b364ded30a55
bao2706 <giabaolop8@gmail.com>
2026-06-27 23:06:01 +0900

    Dictionary export findAll

--- src/main/java/dictionary/controller/DictionaryController.java
+++ src/main/java/dictionary/controller/DictionaryController.java
@@ -5,6 +5,7 @@ import main.java.dictionary.entity.ExampleSentence;
 import main.java.dictionary.entity.Word;
 import main.java.dictionary.request.Request;
 import main.java.dictionary.service.DictionaryService;
+import main.java.dictionary.util.DictionaryExporter;
 
 import java.util.Scanner;
 
@@ -12,65 +13,73 @@ public class DictionaryController {
 
     private Scanner scanner;
     private DictionaryService service;
+    private DictionaryExporter exporter;
 
     public DictionaryController(Scanner scanner, DictionaryService service) {
         this.scanner = scanner;
         this.service = service;
+        this.exporter = new DictionaryExporter();
     }
+
     public void handle(Request request) {
         String action = request.getAction();
-        if (action.equals("lookup") ){
+        if (action.equals("lookup")) {
             handleLookup(request);
             return;
         }
-        if (action.equals("define")){
+        if (action.equals("define")) {
             handleDefine(request);
             return;
         }
-        if (action.equals("drop")){
+        if (action.equals("drop")) {
             handleDrop(request);
             return;
         }
-        System.out.println("chuc nang nay chua ho tro " +action);
+        if (action.equalsIgnoreCase("export")) {
+            handleExport(request);
+            return;
+        }
+        System.out.println("chuc nang nay chua ho tro " + action);
     }
 
     private void handleDrop(Request request) {
         String keyWord = request.getKeyWord();
-        if( keyWord == null || keyWord.isBlank()){
+        if (keyWord == null || keyWord.isBlank()) {
             System.out.println("ban chua nhap tu can xoa");
             return;
         }
         boolean deleted = service.drop(keyWord);
-        if (deleted){
-            System.out.println("@"+keyWord+ " dropped!!");
-        }else {
-            System.out.println("ko tim thay tu "+ keyWord   );
+        if (deleted) {
+            System.out.println("@" + keyWord + " dropped!!");
+        } else {
+            System.out.println("ko tim thay tu " + keyWord);
         }
     }
 
-    private void handleDefine(Request request){
+    private void handleDefine(Request request) {
         String keyWord = request.getKeyWord();
-        if (request.getParams().isEmpty()){
+        if (request.getParams().isEmpty()) {
             System.out.println("chua co dinh nghia");
             return;
         }
         String option = request.getParams().get(0);
         System.out.println("nhap dinh nghia");
-        String sentence  = scanner.nextLine();
+        String sentence = scanner.nextLine();
 
-        ExampleSentence example =null;
-        if (optionCheck(option)){
+        ExampleSentence example = null;
+        if (optionCheck(option)) {
             System.out.println("nhap cau vi du");
             String Example = scanner.nextLine();
-            if (!Example.isBlank()){
+            if (!Example.isBlank()) {
                 System.out.println("nghia cau");
                 String sentenceExample = scanner.nextLine();
                 example =
-                        new ExampleSentence(sentence ,sentenceExample);
+                        new ExampleSentence(sentence, sentenceExample);
             }
+        }
     }
-    }
-    private  boolean optionCheck(String option){
+
+    private boolean optionCheck(String option) {
         return option.equals("-n")
                 || option.equals("--noun")
                 || option.equals("-a")
@@ -78,20 +87,30 @@ public class DictionaryController {
                 || option.equals("-v")
                 || option.equals("--verb");
     }
+
+    private void handleExport(Request request) {
+        String filePath = request.getKeyWord();
+        if (filePath == null || filePath.isEmpty()){
+            System.out.println("vui long nhap duong dan file");
+            return;
+        }
+        exporter.export(filePath,service.findAllWords());
+    }
+
     private void handleLookup(Request request) {
         String keyWord = request.getKeyWord();
         Word word = service.lookup(keyWord);
-        if (keyWord == null){
-            System.out.println("ko tim thay tu" +keyWord);
+        if (keyWord == null) {
+            System.out.println("ko tim thay tu" + keyWord);
             return;
         }
-        System.out.println("@"+word.getKeyWord());
-        for (Definition item :word.getDefinitions()){
+        System.out.println("@" + word.getKeyWord());
+        for (Definition item : word.getDefinitions()) {
             System.out.println(item.getType());
             System.out.println(item.getMeaning());
-            for (ExampleSentence exampleSentence : item.getExamples()){
-                System.out.println(exampleSentence.getMeaning());
-                System.out.println(exampleSentence.getSentence());
+            for (ExampleSentence exampleSentence : item.getExamples()) {
+                System.out.println(" cau "+exampleSentence.getMeaning());
+                System.out.println("nghia "+exampleSentence.getSentence());
             }
         }
     }
--- src/main/java/dictionary/util/DictionaryExporter.java
+++ src/main/java/dictionary/util/DictionaryExporter.java
@@ -1,15 +1,41 @@
 package main.java.dictionary.util;
+
 import java.io.FileWriter;
+
+import main.java.dictionary.entity.ExampleSentence;
+import main.java.dictionary.entity.Word;
+import main.java.dictionary.entity.Definition;
+
 import java.io.IOException;
+import java.util.Dictionary;
+import java.util.List;
+
 public class DictionaryExporter {
-    public void export(String filePath){
+    public void export(String filePath, List<Word> words) {
         try {
-            FileWriter writer= new FileWriter(filePath);
-            writer.write("Dictionary exported!");
+            FileWriter writer = new FileWriter(filePath);
+
+            for (Word word : words) {
+                writeWord(writer, word);
+            }
             writer.close();
             System.out.println("Save thanh cong");
         } catch (IOException e) {
-            System.out.println("ko the xuat file"+ e.getMessage());
+            System.out.println("ko the xuat file" + e.getMessage());
         }
+    }
+
+    private void writeWord(FileWriter writer, Word word)
+            throws IOException {
+        writer.write("@" + word.getKeyWord() + "\n");
+        for (Definition definition : word.getDefinitions()) {
+            writer.write("loai: " + definition.getType() + "\n");
+            writer.write("-" + definition.getMeaning() + "\n");
+            for (ExampleSentence exampleSentence : definition.getExamples()) {
+                writer.write("=" + exampleSentence.getSentence() + "\n");
+                writer.write("=" + exampleSentence.getMeaning() + "\n");
+            }
+        }
+        writer.write("\n");
     }
 }
